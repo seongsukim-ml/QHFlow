4edace7f394e3746d32f252beca1ec583fbaf379
holywater <seongsukim@postech.ac.kr>
2025-08-14 14:50:27 +0900

    upload water

---
+++ _my_scripts/upload_md17.sh
@@ -0,0 +1,4 @@
+cd $HOME/25DFT/QHFlow/dataset
+$HOME/gdrive files upload --recursive --chunk-size 256 --print-chunk-errors --print-chunk-info --parent 1d3HTu0H7gdg54kirWBqN24x-s1QW6OKV uracil
+$HOME/gdrive files upload --recursive --chunk-size 256 --print-chunk-errors --print-chunk-info --parent 1d3HTu0H7gdg54kirWBqN24x-s1QW6OKV water 
+$HOME/gdrive files upload --recursive --chunk-size 256 --print-chunk-errors --print-chunk-info --parent 1d3HTu0H7gdg54kirWBqN24x-s1QW6OKV malondialdehyde
