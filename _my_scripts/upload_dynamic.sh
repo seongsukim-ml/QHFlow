c8c26d88a94ef1ec7b5c9e3ca1f62fab0f5b87b6
holywater <seongsukim@postech.ac.kr>
2025-08-17 04:13:40 +0900

    Add upload functionality for QH9Dynamic.lmdb to upload_dynamic.sh

--- _my_scripts/upload_dynamic.sh
+++ _my_scripts/upload_dynamic.sh
@@ -13,3 +13,23 @@ $HOME/_my_initial/gdrive files upload --recursive --chunk-size 256 \
     --print-chunk-errors --print-chunk-info \
     --parent 19-Hi2VpcI2YcZBweMeiRgU9iBLlwkK8a \
     QH9Dynamic_300k/processed/QH9Dynamic_1_completed.lmdb
+
+cd $HOME/25DFT/QHFlow/dataset && \
+$HOME/gdrive files upload --recursive --chunk-size 256 \
+    --print-chunk-errors --print-chunk-info \
+    --parent 1sPZ81P_wv6DRXnops3Lj-QYW9f4_NiNr \
+    QH9Dynamic_300k/processed/QH9Dynamic.lmdb
+
+cd $HOME/25DFT/QHFlow/dataset && \
+$HOME/gdrive files upload --recursive --chunk-size 256 \
+    --print-chunk-errors --print-chunk-info \
+    --parent 1sPZ81P_wv6DRXnops3Lj-QYW9f4_NiNr \
+    QH9Dynamic_300k/processed/processed_QH9Dynamic_mol.pt
+
+    
+
+cd $HOME/25DFT/QHFlow/dataset && \
+$HOME/gdrive files upload --recursive --chunk-size 256 \
+    --print-chunk-errors --print-chunk-info \
+    --parent 1cW7LngAWTGEdOj_SB9da2_R3uPCVuf6j \
+    QH9Stable/processed/processed_QH9Stable_size_ood.pt
